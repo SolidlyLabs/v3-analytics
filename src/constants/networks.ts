e6432d7e544d5bf37f9895e078a4874a6aed6aba
NindoK <b.daniele@protonmail.ch>
2024-04-05 01:06:23 +0200

    Add Omnichain logo and update Omnichain network image URL

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,6 +1,7 @@
 import OPTIMISM_LOGO_URL from '../assets/images/optimism.svg'
 import ARBITRUM_LOGO_URL from '../assets/images/arbitrum.svg'
 import ETHEREUM_LOGO_URL from '../assets/images/ethereum-logo.png'
+import OMNICHAIN_LOGO_URL from '../assets/images/omnichain-logo3.png'
 import FANTOM_LOGO_URL from '../assets/images/fantom-logo.png'
 import POLYGON_LOGO_URL from '../assets/images/polygon-logo.png'
 import CELO_LOGO_URL from '../assets/images/celo-logo.svg'
@@ -64,7 +65,7 @@ export const OmnichainNetworkInfo: NetworkInfo = {
   bgColor: '#2dcb4b',
   primaryColor: '#2dcb4b',
   secondaryColor: '#ffffff',
-  imageURL: ETHEREUM_LOGO_URL,
+  imageURL: OMNICHAIN_LOGO_URL,
 }
 
 export const EthereumNetworkInfo: NetworkInfo = {
